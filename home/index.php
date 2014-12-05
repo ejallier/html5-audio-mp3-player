23ca692db327075f9a90498b00f5f16d0d953dd3
Erwann Jallier <erwann.jallier@gmail.com>
2014-12-05 11:37:13 +0100

    a big clean_up to begin

--- home/index.php
+++ home/index.php
@@ -1,21 +1,19 @@
 <?php
-include ("../inc/top.inc.php");
+	include ("../inc/top.inc.php");
 
-require_once('../inc/getid3/getid3.php');
+	require_once('../inc/getid3/getid3.php');
 
-// Initialize getID3 engine
-$getID3 = new getID3;
-$mp3 = "../music/zik2.mp3";
-// Analyze file and store returned data in $ThisFileInfo
-$ThisFileInfo = $getID3->analyze($mp3);
+	// Initialize getID3 engine
+	$getID3 = new getID3;
+	$mp3 = "../music/zik2.mp3";
+	// Analyze file and store returned data in $ThisFileInfo
+	$ThisFileInfo = $getID3->analyze($mp3);
 
-getid3_lib::CopyTagsToComments($ThisFileInfo);
+	getid3_lib::CopyTagsToComments($ThisFileInfo);
 
-
-$titre =  $ThisFileInfo['tags_html']['id3v1']['title'][0];  // title from ID3v2
-$artiste =  $ThisFileInfo['tags_html']['id3v1']['artist'][0];  // title from ID3v2
-$album =  $ThisFileInfo['tags_html']['id3v1']['album'][0];  // title from ID3v2
-
+	$titre =  $ThisFileInfo['tags_html']['id3v1']['title'][0];  // title from ID3v1
+	$artiste =  $ThisFileInfo['tags_html']['id3v1']['artist'][0];  // title from ID3v1
+	$album =  $ThisFileInfo['tags_html']['id3v1']['album'][0];  // title from ID3v1
 
 ?>
 	
@@ -33,17 +31,6 @@ $album =  $ThisFileInfo['tags_html']['id3v1']['album'][0];  // title from ID3v2
 			data-artiste="<?= $artiste ?>"
 			data-album="<?= $album ?>">
 		</div>
-
-		<h2>RESTE A FAIRE</h2>
-		<ul>
-			<li>curseur sur timeline</li>
-			<li>Créer balises Player par JS</li>
-			<li>Récup données par PhP</li>
-			<li>Les récup en JSON</li>
-			<li>Les afficher par JS</li>
-			
-		</ul>
-		
 		
 		
 	</div> <!-- /#content -->
--- inc/top.inc.php
+++ inc/top.inc.php
@@ -1,6 +1,4 @@
-<?php
-include ("../inc/functions.inc.php");
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <!--[if lte IE 7]> <html class="oldie ie" lang="fr"> <![endif]-->
 <!--[if IE 8]> <html class="ie8 ie" lang="fr"> <![endif]-->
 <!--[if gt IE 8]><!--> <html lang="fr"> <!--<![endif]-->
@@ -9,13 +7,9 @@ include ("../inc/functions.inc.php");
 	<meta charset="UTF-8">
 
 	<title>Test Audio</title>
-
-	<!--[if lt IE 9]>
-		<script src="../js/html5shiv.js"></script>
-	<![endif]-->
 	
 	
-	<link rel="stylesheet" href="../style/reset.css">
+	<link rel="stylesheet" href="../style/normalize.css">
 	<link rel="stylesheet" href="../style/style.css">
 	<link rel="stylesheet" href="../style/audio.css">
 </head>
